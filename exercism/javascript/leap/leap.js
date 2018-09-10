be6dddeda83be726d59fef1c91e4cd516820beec
gcoronado28 <gabrielomar2809@gmail.com>
2018-09-10 16:29:26 -0400

    Leap

--- exercism/javascript/leap/leap.js
+++ exercism/javascript/leap/leap.js
@@ -1,6 +1,6 @@
 // Given a year, report if it is a leap year.
 export function isLeap(year) {
-	var f = (year % 4) === 0 && (year % 100) !== 0
-	var s = (year % 100) === 0 && (year % 400) === 0
-	return f || s;
+  let f = (year % 4) === 0 && (year % 100) !== 0;
+  let s = (year % 100) === 0 && (year % 400) === 0;
+  return f || s;
 }
