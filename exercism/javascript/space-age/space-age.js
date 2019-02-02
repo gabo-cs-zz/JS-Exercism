56951c914483ffc0021cf5c7c1c5429e4751f500
gcoronado28 <gabrielomar2809@gmail.com>
2019-02-01 22:04:33 -0500

    Space Age v2. Some metaprogramming

--- exercism/javascript/space-age/space-age.js
+++ exercism/javascript/space-age/space-age.js
@@ -1,45 +1,27 @@
 // Given an age in seconds, calculate how old someone would be on planet age
 const EARTH_SECS = 31557600;
 
-const result = (seconds, orbitalPeriod) => {
-  const NUM = seconds / (EARTH_SECS * orbitalPeriod);
-  return Number(parseFloat(NUM).toFixed(2));
+const PLANETS = {
+  Earth: 1,
+  Mercury: 0.2408467,
+  Venus: 0.61519726,
+  Mars: 1.8808158,
+  Jupiter: 11.862615,
+  Saturn: 29.447498,
+  Uranus: 84.016846,
+  Neptune: 164.79132,
+};
+
+const calculation = (seconds, orbitalPeriod) => {
+  const num = seconds / (EARTH_SECS * orbitalPeriod);
+  return Number(num.toFixed(2));
 };
 
 export class SpaceAge {
   constructor(secs) {
     this.seconds = secs;
-  }
-
-  onEarth() {
-    return result(this.seconds, 1);
-  }
-
-  onMercury() {
-    return result(this.seconds, 0.2408467);
-  }
-
-  onVenus() {
-    return result(this.seconds, 0.61519726);
-  }
-
-  onMars() {
-    return result(this.seconds, 1.8808158);
-  }
-
-  onJupiter() {
-    return result(this.seconds, 11.862615);
-  }
-
-  onSaturn() {
-    return result(this.seconds, 29.447498);
-  }
-
-  onUranus() {
-    return result(this.seconds, 84.016846);
-  }
-
-  onNeptune() {
-    return result(this.seconds, 164.79132);
+    for (let [planet, period] of Object.entries(PLANETS)) {
+      this[`on${planet}`] = () => calculation(this.seconds, period);
+    }
   }
 }
