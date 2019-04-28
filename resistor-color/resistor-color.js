c61ee81724bd24fdf1348fc31714d735a72436f7
gcoronado28 <gabrielomar2809@gmail.com>
2019-04-28 23:26:50 +0000

    Resistor Color

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,5 @@
+const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+
+const colorCode = (input) => COLORS.indexOf(input);
+
+export { colorCode, COLORS };
